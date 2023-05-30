f1bf9928fe9b12d2d3387cf2b2a33e6c12268f7d
studioValenz <studiovalenz@gmail.com>
2023-05-30 17:45:40 -0300

    Aprendendo React native

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    let nome = "Julio Anderson";
+    let img = "https://sujeitoprogramador.com/reactlogo.png";
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Testando</Text>
+        <Text style={{ fontSize: 30 }}>Meu primeiro app</Text>
+
+        <Image style={styles.img} source={{ uri: img }} />
+
+        <Text style={styles.text}>{nome}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 30,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    color: "#254df2",
+    backgroundColor: "black",
+  },
+  img: {
+    width: 300,
+    height: 300,
   },
 });
